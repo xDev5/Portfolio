fb453184c61d53fb363120b17e8661e91a292b55
xDev5 <110704391+xDev5@users.noreply.github.com>
2022-08-15 17:46:35 -0700

    First attempt at js FAILURE! it works in js.fiddle
    i will revisit this at a later date

---
+++ letswork/script/app.js
@@ -0,0 +1,27 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft =  document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+
+let result = 0
+let timerId = null 
+
+function randomSquare(){
+squares.forEach (square => {
+   square.classList.remove('mole')
+  
+})
+
+let randomSquare = squares[Math.floor(Math.random() * 9)]
+
+randomSquare.classList.add('mole')
+
+
+}
+
+function moveMole(){
+
+timerId = setInterval(randomSquare, 500)
+}
+moveMole()
